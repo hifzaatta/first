050cefb2832cdffd401edfedbacfaa1a510904bc
hifzaatta <ratta2778@gmail.com>
2024-08-26 06:26:14 -0700

    add myprogram.dart(changes), week2.dart(new)

--- week2.dart
+++ week2.dart
@@ -1,10 +1,10 @@
 void main() {
   // print(5 / 2);
-  var names = ['izna', 'shafeeq', 'ahmed'];
+  var names = ['izna', 'hifza', 'fizza'];
   print(names[1]);
   print(names.length);
   print(names.elementAt(0));
-  names.replaceRange(names.length-1,names.length,['imuu']);
+  names.replaceRange(names.length-1,names.length,['rabia']);
   print(names);
   // print(names.sort);
 }
