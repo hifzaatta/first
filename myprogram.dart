c90baa5e35ede9b4a809d157b01a249ca57779c6
hifzaatta <ratta2778@gmail.com>
2024-08-25 22:07:08 -0700

    add myprogram.dart(changes), week2.dart(new)

---
+++ myprogram.dart
@@ -0,0 +1,68 @@
+// // void main() {
+// //   print("hi");
+// //   var fname = "hifza";
+// //   var lname = "atta";
+// //   print(fname+' '+lname);
+// // }
+
+// // import 'dart:io';
+
+// // void main(List<String> args)  {
+// //   stdout.writeln("what is your name?");
+// //   String? name = stdin.readLineSync();
+// //   print("your name is $name");
+// // }
+// // void main(List<String> args) {
+// //   int;
+// //   double;
+// //   String;
+// //   bool;
+// //   dynamic;
+// //   var amount1 = 100;
+// //   int amount2 = 200;
+// //   print("amount1:$amount1 + $amount2");
+
+// //   var s1 = 'simple string in single commas';
+// //   var s2 = "simple string in double commas";
+
+// // }
+// //
+
+// class Num {
+//   int num = 10;
+// }
+
+// void main(List<String> args) {
+//   // var numbers = [1, 2, 3, 4];
+
+//   // for (var n in numbers) print(n);
+//   // for (var i = 0; i < numbers.length; i++) print(numbers[i]);
+//   // numbers.forEach((n) => print(n));
+//   // numbers.forEach(printnum);
+//   //  as we add function so we write this function outside the main
+
+//   // collection of lists set
+//   // var names = ['Hifza Atta', 'Izna Rasool', 'Aiman Munir',19,21.5];
+//   // List<String> names = ['Hifza Atta', 'Izna Rasool', 'Aiman Munir'];
+//   // names[2] = 'alina';
+//   // for (var n in names) print(n);
+//   // we can copy one list to another
+//   // var names1 = names;
+//   // names[1] = 'maira';
+
+//   // for (var n in names1) print(n);
+// //   var x=<String>[];
+// //    x=['hadia','noor','noor'];
+// // for(var i in x)
+// //   print(i);
+//   // var prize = [
+//   //   'first':'gold',
+//   //   'second':'silver',
+//   //   'third':'bronze'];
+//   // print(prize['third']);
+  
+
+
+// // void printnum(num) {
+// //   print(num);
+//  }
---
+++ week2.dart
@@ -0,0 +1,10 @@
+void main() {
+  // print(5 / 2);
+  var names = ['izna', 'shafeeq', 'ahmed'];
+  print(names[1]);
+  print(names.length);
+  print(names.elementAt(0));
+  names.replaceRange(names.length-1,names.length,['imuu']);
+  print(names);
+  // print(names.sort);
+}
